4f1856e24c91e83f34339e0c954063cfb4af3e17
Jaros <jaros@chimplie.com>
2025-01-04 19:46:04 +0100

    add looping triangle

---
+++ eloquent-js/looping-triangle.js
@@ -0,0 +1,12 @@
+
+let brush = "";
+for (let i = 0; i < 7; i++) {
+    brush += "#";
+    console.log(brush);
+}
+
+
+for (let line = "#"; line.length < 8; line += "#") {
+    console.log(line);
+}
+    
