8a73cb393ce34d8c53b463e2a9e551b0483eca29
Jaros <jaros@chimplie.com>
2025-01-04 20:13:43 +0100

    add FizzBuzz

--- eloquent-js/looping-triangle.js
+++ eloquent-js/looping-triangle.js
@@ -1,12 +1,11 @@
-
+//my initial approach...
 let brush = "";
 for (let i = 0; i < 7; i++) {
     brush += "#";
     console.log(brush);
 }
 
-
+//a smarter approach...
 for (let line = "#"; line.length < 8; line += "#") {
     console.log(line);
 }
-    
